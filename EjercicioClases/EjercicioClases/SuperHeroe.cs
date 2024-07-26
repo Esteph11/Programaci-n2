41af43ca8037f58f47bd86c1006dd2d59631e68f
Esteph11 <esteportilloluna11@gmail.com>
2024-07-26 16:06:56 -0600

    SuperHeroe1.1

---
+++ EjercicioClases/EjercicioClases/SuperHeroe.cs
@@ -0,0 +1,34 @@
+﻿
+namespace EjercicioClases
+{
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+
+        public String IdentidadSecreta { get; set; }
+
+        public String Ciudad { get; set; }
+
+        public bool PuedeVolar { get; set; }
+
+        public string SuperPoder { get; set; }
+
+        // Constructor
+        public SuperHeroe()
+        {
+            PuedeVolar = true;
+            SuperPoder = new SuperPoder();
+        }
+
+            public void Imprimir()
+            {
+                Console.WriteLine($"Nombre: {Nombre}");
+                Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+                Console.WriteLine($"Ciudad: {Ciudad}");
+                Console.WriteLine($"Puede Volar: {(PuedeVolar ? "Sí" : "No")}");
+                Console.WriteLine($"Super Poder: {SuperPoder}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
